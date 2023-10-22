2502b45858aa7e43cb94b11e497278e11baa8b5e
wufeichenxin <wufeichenxin@gmail.com>
2023-10-22 19:49:11 +0800

    首次提交

---
+++ app/src/main/java/com/zymanger/gittest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.zymanger.gittest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //第一次提交测试 19.48
+    }
+}
